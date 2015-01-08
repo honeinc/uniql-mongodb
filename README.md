e6e421ff2afbb53ade47c6655fc4f9b4f11f298a
Andy Burke <aburke@bitflood.org>
2015-01-07 18:22:40 -0800

    Use comparison extraction, update README.

--- README.md
+++ README.md
@@ -3,6 +3,25 @@ UniQL-MongoDB
 
 This generates MongoDB queries based on [UniQL](https://github.com/honeinc/uniql) ASTs.
 
+## Example
+
+```javascript
+var parse = require( 'uniql' );
+var mongoCompile = require( 'uniql-mongodb' );
+
+var ast = parse( '( height <= 20 or ( favorites.color == "green" and height != 25 ) ) and firstname ~= "o.+"' );
+var mongoQuery = mongoCompile( ast );
+console.log( util.inspect( mongoQuery, { depth: null } ) );
+```
+
+Resulting query:
+
+```
+{ '$or': 
+   [ { height: { '$lte': 20 } },
+     { 'favorites.color': 'green', height: { '$ne': 25 } } ],
+  firstname: { '$regex': 'o.+' } }
+```
 
 ## License
 
--- src/compiler.js
+++ src/compiler.js
@@ -18,15 +18,15 @@ var generators = {
     },
     "&&": function( node ) {
         var result = [];
-        node.arguments.forEach( function( arg ) {
-           result.push( _processNode( arg ) );
+        node.arguments.forEach( function( _node ) {
+           result.push( _processNode( _node ) );
         } );
         return ' ' + result.join( ',\n' );
     },
     "||": function( node ) {
         var result = [];
-        node.arguments.forEach( function( arg ) {
-            result.push( ' { ' + _processNode( arg ) + '}' );
+        node.arguments.forEach( function( _node ) {
+            result.push( ' { ' + _processNode( _node ) + '}' );
         } );
         return ' "$or": [ ' + result.join( ',\n' ) + ' ]\n'; 
     },
@@ -39,55 +39,68 @@ var generators = {
         throw new Error( '! operator not supported by mongodb' );
     },
     "==": function( node ) {
-        if ( node.arguments[ 0 ].type !== 'SYMBOL' ) {
-            throw new Error( 'mongodb only supports left hand side symbols in equality tests' );
-        }
-        return ' ' + _processNode( node.arguments[ 0 ] ) + ': ' + _processNode( node.arguments[ 1 ] ) + ' ';
+        var comparison = _extractComparison( node );
+        return ' ' + _processNode( comparison.symbol ) + ': ' + _processNode( comparison.value ) + ' ';
     },
     "!=": function( node ) {
-        if ( node.arguments[ 0 ].type !== 'SYMBOL' ) {
-            throw new Error( 'mongodb only supports left hand side symbols in equality tests' );
-        }
-        return ' ' + _processNode( node.arguments[ 0 ] ) + ': { "$ne": ' + _processNode( node.arguments[ 1 ] ) + ' } ';
+        var comparison = _extractComparison( node );
+        return ' ' + _processNode( comparison.symbol ) + ': { "$ne": ' + _processNode( comparison.value ) + ' } ';
     },
     "MATCH": function( node ) {
-        if ( node.arguments[ 0 ].type !== 'SYMBOL' ) {
-            throw new Error( 'mongodb only supports left hand side symbols in matching tests' );
-        }
-        return ' ' + _processNode( node.arguments[ 0 ] ) + ': { "$regex": ' + _processNode( node.arguments[ 1 ] ) + ' } ';
+        var comparison = _extractComparison( node );
+        return ' ' + _processNode( comparison.symbol ) + ': { "$regex": ' + _processNode( comparison.value ) + ' } ';
     },
     "<": function( node ) {
-        if ( node.arguments[ 0 ].type !== 'SYMBOL' ) {
-            throw new Error( 'mongodb only supports left hand side symbols in comparison tests' );
-        }
-        return ' ' + _processNode( node.arguments[ 0 ] ) + ': { "$lt": ' + _processNode( node.arguments[ 1 ] ) + ' } ';
+        var comparison = _extractComparison( node );
+        return ' ' + _processNode( comparison.symbol ) + ': { "$lt": ' + _processNode( comparison.value ) + ' } ';
     },    
     "<=": function( node ) {
-        if ( node.arguments[ 0 ].type !== 'SYMBOL' ) {
-            throw new Error( 'mongodb only supports left hand side symbols in comparison tests' );
-        }
-        return ' ' + _processNode( node.arguments[ 0 ] ) + ': { "$lte": ' + _processNode( node.arguments[ 1 ] ) + ' } ';
+        var comparison = _extractComparison( node );
+        return ' ' + _processNode( comparison.symbol ) + ': { "$lte": ' + _processNode( comparison.value ) + ' } ';
     },    
     ">": function( node ) {
-        if ( node.arguments[ 0 ].type !== 'SYMBOL' ) {
-            throw new Error( 'mongodb only supports left hand side symbols in comparison tests' );
-        }
-        return ' ' + _processNode( node.arguments[ 0 ] ) + ': { "$gt": ' + _processNode( node.arguments[ 1 ] ) + ' } ';
+        var comparison = _extractComparison( node );
+        return ' ' + _processNode( comparison.symbol ) + ': { "$gt": ' + _processNode( comparison.value ) + ' } ';
     },    
     ">=": function( node ) {
-        if ( node.arguments[ 0 ].type !== 'SYMBOL' ) {
-            throw new Error( 'mongodb only supports left hand side symbols in comparison tests' );
-        }
-        return ' ' + _processNode( node.arguments[ 0 ] ) + ': { "$gte": ' + _processNode( node.arguments[ 1 ] ) + ' } ';
+        var comparison = _extractComparison( node );
+        return ' ' + _processNode( comparison.symbol ) + ': { "$gte": ' + _processNode( comparison.value ) + ' } ';
     },
     "EXPRESSION": function( node ) {
         var result = [];
-        node.arguments.forEach( function( arg ) {
-            result.push( _processNode( arg ) );
+        node.arguments.forEach( function( _node ) {
+            result.push( _processNode( _node ) );
         } );
         return ' ' + result.join( ',\n' );        
     }
 };
+
+function _extractComparison( node ) {
+    var symbol = null;
+    var value = null;
+    node.arguments.forEach( function( _node ) {
+        if ( _node.type === 'SYMBOL' ) {
+            if ( symbol ) {
+                throw new Error( 'MONGODB: You can only specify one symbol in a comparison.' );
+            }
+            symbol = _node;
+        } else {
+            if ( value ) {
+                throw new Error( 'MONGODB: You can only specify one value in a comparison.' );
+            }
+            value = _node;
+        }
+    } );
+
+    if ( !( symbol && value ) ) {
+        throw new Error( 'MONGODB: Invalid comparison, could not find both symbol and value.' );
+    }
+
+    return {
+        symbol: symbol,
+        value: value
+    };
+}
 
 function _processNode( node ) {
     if ( !( node.type in generators ) ) {
